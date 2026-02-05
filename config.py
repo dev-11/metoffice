e2518d216f81d79c8625bef10c63ee74e816880a
Otto <otto@masterbranch.io>
2026-02-05 11:50:41 +0100

    init version

---
+++ config.py
@@ -0,0 +1,4 @@
+medical_meteorology_url = "https://www.hazipatika.com/orvosmeteorologia"
+anchor = ".o-humanMeteoDay"
+front_type_class = ".o-humanMeteoDay__frontEffect"
+datetime_class = ".o-humanMeteoDay__datedata"
--- entry_point/app.py
+++ entry_point/app.py
@@ -1,6 +1,30 @@
+import re
+
+import bs4
+import requests
+import config as c
+
+
 def lambda_handler(event, context):
-    # TODO implement
+
+    page = requests.get(c.medical_meteorology_url)
+
+    soup = bs4.BeautifulSoup(page.text, "html.parser")
+
+    days = soup.select(c.anchor)
+
+    results = []
+    for day in days:
+        front_type_el = day.select_one(c.front_type_class)
+        datetime_el = day.select_one(c.datetime_class)
+
+        if front_type_el and datetime_el:
+            front_type = front_type_el.text.strip()
+            datetime = re.sub(r"\s+", " ", datetime_el.text.strip())
+
+            results.append((front_type, datetime))
+
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda!'
+        'body': results
     }

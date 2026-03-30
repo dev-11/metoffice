e5db648c31897efe1408858031f73e8897a8236e
Otto <otto@masterbranch.io>
2026-03-30 15:39:24 +0200

    updating cicd

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,8 @@
 """Services to collect all the data for the lambda."""
 from .service_factory import ServiceFactory
 from .scraper_service import ScraperService
+
+__all__ = [
+    'ServiceFactory',
+    'ScraperService'
+]
